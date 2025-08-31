868b977d50f2f888781a6ad5e8289532e6bb6da2
Allan <nabuleallan1@gmail.com>
2025-08-31 20:00:10 +0300

    added clean customers

--- crm/settings.py
+++ crm/settings.py
@@ -135,3 +135,14 @@ CRONJOBS = [
     ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
     ('0 */12 * * *', 'crm.cron.update_low_stock'),
 ]
+
+from celery.schedules import crontab
+
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+
+CELERY_BEAT_SCHEDULE = {
+    'generate-crm-report': {
+        'task': 'crm.tasks.generate_crm_report',
+        'schedule': crontab(day_of_week='mon', hour=6, minute=0),  # every Monday 6:00 AM
+    },
+}
